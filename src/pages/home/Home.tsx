97f87479efc25caa61cc33d49b63c47abddd3b9c
erick-costa <erickcosta2509@gmail.com>
2024-10-17 16:06:02 -0300

    feat: adding home page

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,3 +1,19 @@
+import { Button, Flex, Heading } from "@radix-ui/themes"
+import { Link } from "react-router-dom"
+
 export function Home() {
-  return <h1>Home</h1>
+  return (
+    <Flex
+      mt={"9"}
+      justify="center"
+      direction={"column"}
+      align={"center"}
+      gap={"4"}
+    >
+      <Heading>Bem vindo(a)!</Heading>
+      <Link to="/register">
+        <Button>Cadastrar cliente</Button>
+      </Link>
+    </Flex>
+  )
 }
