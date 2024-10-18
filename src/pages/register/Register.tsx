e9eee6a93446f2763b6c988dad7adeb1abb6f272
erick-costa <erickcosta2509@gmail.com>
2024-10-18 08:39:06 -0300

    feat: finishing register form layout

--- src/pages/register/Register.tsx
+++ src/pages/register/Register.tsx
@@ -14,42 +14,49 @@ export function Register() {
     <>
       <Heading mt={"9"}>Cadastrar cliente</Heading>
 
-      <Flex align="center" direction="column">
+      <Flex align="center" direction="column" mb="9">
         <Separator my="6" size="4" />
         <Flex width="60%" direction="column" gap="3">
           <label>
             <Text as="div" size="2" mb="2" weight="bold">
               CPF
             </Text>
-            <TextField.Root placeholder="Digite seu CPF" />
+            <TextField.Root required placeholder="Digite seu CPF" />
           </label>
 
           <label>
             <Text as="div" size="2" mb="2" weight="bold">
               Nome
             </Text>
-            <TextField.Root placeholder="Digite seu nome" />
+            <TextField.Root required placeholder="Digite seu nome" />
           </label>
 
           <label>
             <Text as="div" size="2" mb="2" weight="bold">
               Email
             </Text>
-            <TextField.Root type="email" placeholder="Digite seu e-mail" />
+            <TextField.Root
+              required
+              type="email"
+              placeholder="Digite seu e-mail"
+            />
           </label>
 
           <label>
             <Text as="div" size="2" mb="2" weight="bold">
               Telefone
             </Text>
-            <TextField.Root placeholder="Digite seu número de telefone/celular" />
+            <TextField.Root
+              required
+              placeholder="Digite seu número de telefone/celular"
+            />
           </label>
 
           <label>
             <Text as="div" size="2" mb="2" weight="bold">
               Data de nascimento
             </Text>
-            <TextField.Root type="date" />
+            <TextField.Root required type="date" />
           </label>
 
           <Text as="label" size="2" mb="4">
@@ -75,7 +82,7 @@ export function Register() {
             <Text as="div" mb="2" size="2" weight="bold">
               Tipo de endereço
             </Text>
-            <Select.Root defaultValue="home">
+            <Select.Root required defaultValue="home">
               <Select.Trigger />
               <Select.Content>
                 <Select.Group>
@@ -85,14 +92,62 @@ export function Register() {
               </Select.Content>
             </Select.Root>
           </label>
+
+          <label>
+            <Text as="div" size="2" mb="2" weight="bold">
+              Endereço
+            </Text>
+            <TextField.Root required placeholder="Digite seu endereço" />
+          </label>
+
+          <label>
+            <Text as="div" size="2" mb="2" weight="bold">
+              Número
+            </Text>
+            <TextField.Root
+              required
+              placeholder="Digite o número do seu endereço"
+            />
+          </label>
+
+          <label>
+            <Text as="div" size="2" mb="2" weight="bold">
+              Complemento
+            </Text>
+            <TextField.Root placeholder="Digite o complemento" />
+          </label>
+
+          <label>
+            <Text as="div" size="2" mb="2" weight="bold">
+              Bairro
+            </Text>
+            <TextField.Root placeholder="Digite seu bairro" />
+          </label>
+
+          <label>
+            <Text as="div" size="2" mb="2" weight="bold">
+              Cidade
+            </Text>
+            <TextField.Root placeholder="Digite sua cidade" />
+          </label>
+
+          <label>
+            <Text as="div" size="2" mb="2" weight="bold">
+              Estado
+            </Text>
+            <TextField.Root placeholder="Digite seu estado" />
+          </label>
+
+          <label>
+            <Text as="div" size="2" mb="2" weight="bold">
+              CEP
+            </Text>
+            <TextField.Root placeholder="Digite seu CEP" />
+          </label>
         </Flex>
 
-        <Flex width="60%" gap="3" mt="4" justify="end">
-          <Button variant="soft" color="gray">
-            Cancel
-          </Button>
-
-          <Button>Save</Button>
+        <Flex width="60%" mt="4" justify="end">
+          <Button>Cadastrar</Button>
         </Flex>
       </Flex>
     </>
