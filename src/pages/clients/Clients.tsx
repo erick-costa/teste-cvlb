52facbe40a8eedfb1a1614598d1c7b34d4377315
erick-costa <erickcosta2509@gmail.com>
2024-10-18 11:04:51 -0300

    feat: clients list layout

--- src/pages/clients/Clients.tsx
+++ src/pages/clients/Clients.tsx
@@ -1,3 +1,22 @@
+import { Button, Card, Flex, Heading, Separator, Text } from "@radix-ui/themes"
+
 export function Clients() {
-  return <h1>Clientes</h1>
+  return (
+    <>
+      <Heading mt={"9"}>Lista de clientes</Heading>{" "}
+      <Separator my="6" size="4" />
+      <Flex wrap="wrap" gap="6">
+        <Card>
+          <Text as="div" size="2" weight="bold">
+            Teodros Girmay
+          </Text>
+          <Text as="div" size="2" color="gray">
+            teodros@gmail.com
+          </Text>
+          <Separator my="2" size="4" />
+          <Button>Mais informações</Button>
+        </Card>
+      </Flex>
+    </>
+  )
 }
